5ad169be9da2a4401e67cf6f3f4a83fa81e17b6c
Abdurrahman Rahim <104781398+abdurrahmanr@users.noreply.github.com>
2022-12-04 17:42:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav-menu");
+const gotop = document.querySelector(".goTop");
 
 menu.addEventListener("click", () =>{
     menu.classList.toggle("active");
@@ -10,3 +11,16 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     menu.classList.remove("active");
     nav.classList.remove("active");
 }))
+
+document.querySelector(".logo").addEventListener("click", () =>{
+    menu.classList.remove("active");
+    nav.classList.remove("active");
+})
+
+window.addEventListener("scroll", () =>{
+    if(window.pageYOffset > 100){
+        gotop.classList.add("active");
+    } else {
+        gotop.classList.remove("active");
+    }
+})
